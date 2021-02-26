6008578c8a059233685071d78a7629656fb31a7a
Nyi Nyi Lwin <53917354+mdynnl@users.noreply.github.com>
2021-02-26 18:42:57 +0630

    Updates

--- dist/App.svelte.js
+++ dist/App.svelte.js
@@ -231,7 +231,7 @@ function instance($$self, $$props, $$invalidate) {
 	}
 
 	const only_handler = () => {
-		$$invalidate(0, active++, active);
+		if (active === lessons.length) $$invalidate(0, active = 0); else $$invalidate(0, active++, active);
 		$$invalidate(1, input = "");
 	};
 
